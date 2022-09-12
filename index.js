12c485b6f2034cef38be030519016d5f791381ca
kapilgoel533 <kapilgoel533@gmail.com>
2022-09-12 14:41:31 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,95 @@
+const express = require("express");
+const paypal = require("paypal-rest-sdk");
+
+paypal.configure({
+  mode: "sandbox", //sandbox or live
+  client_id:
+    "AaWQSsw8-Pf15jr3lZZ2gcGjn3XZHk9_OdJgDI5AKODcy18_Gw-3pOVHOxVTNwfWLj5jFOLzmeHiDSf7",
+  client_secret:
+    "EFl7mXSY6pm8Z-cWHdJaEGKkZspJl7kOLDmixxyvaylsSrrunpdC8u9YZWO0bHKBWfLwOdNhtld-0L0w",
+});
+
+const PORT = process.env.PORT | 3001;
+const app = express();
+
+app.get("/", (req, res) => res.sendFile(__dirname + "/index.html"));
+
+app.post("/pay", (req, res) => {
+  const create_payment_json = {
+    intent: "sale",
+    payer: {
+      payment_method: "paypal",
+    },
+    redirect_urls: {
+      return_url: "http://localhost:3001/success",
+      cancel_url: "http://localhost:3001/cancel",
+    },
+    transactions: [
+      {
+        item_list: {
+          items: [
+            {
+              name: "Red Sox Hat",
+              sku: "001",
+              price: "5.00",
+              currency: "USD",
+              quantity: 1,
+            },
+          ],
+        },
+        amount: {
+          currency: "USD",
+          total: "5.00",
+        },
+        description: "Hat for the best team ever",
+      },
+    ],
+  };
+
+  paypal.payment.create(create_payment_json, (error, payment) => {
+    if (error) {
+      throw error;
+    } else {
+      for (let i = 0; i < payment.links.length; i++) {
+        if (payment.links[i].rel === "approval_url") {
+          res.redirect(payment.links[i].href);
+        }
+      }
+    }
+  });
+});
+
+app.get("/success", (req, res) => {
+  const payerId = req.query.PayerID;
+  const paymentId = req.query.paymentId;
+
+  const execute_payment_json = {
+    payer_id: payerId,
+    transactions: [
+      {
+        amount: {
+          currency: "USD",
+          total: "5.00",
+        },
+      },
+    ],
+  };
+
+  paypal.payment.execute(
+    paymentId,
+    execute_payment_json,
+    function (error, payment) {
+      if (error) {
+        console.log(error.response);
+        throw error;
+      } else {
+        console.log(JSON.stringify(payment));
+        res.send("Success");
+      }
+    }
+  );
+});
+
+app.get("/cancel", (req, res) => res.send("Cancelled"));
+
+app.listen(PORT, () => console.log(`Server Started on ${PORT}`));
